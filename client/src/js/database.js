3a2fa3649dc119230f2c8a1e4191e6dd82a5e662
benvjohn <benvjohn@gmail.com>
2023-11-21 18:53:54 -0600

    fixed databasejs

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -26,7 +26,7 @@ export const putDb = async (content) => {
   const store = tx.objectStore('jate');
 
   // Use the .add() method on the store and pass in the content.
-  const request = store.add({content});
+  const request = store.put({content, id:1});
 
   // Get confirmation of the request.
   const result = await request;
@@ -47,7 +47,7 @@ export const getDb = async () => {
   const store = tx.objectStore('jate');
 
   // Use the .getAll() method to get all data in the database.
-  const request = store.getAll();
+  const request = store.get(1);
 
   // Get confirmation of the request.
   const result = await request;
